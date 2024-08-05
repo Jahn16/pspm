1e31fe530c16577d5e062ad395de48844d0965f5
Jahn16 <jpdeoliveiraferreira@gmail.com>
2024-08-05 00:26:27 -0300

    chore: add flakes

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "A Nix-flake-based Python development environment";
+
+  inputs = {
+    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
+    pre-commit-hooks.url = "github:Jahn16/git-hooks.nix";
+  };
+  outputs = { self, nixpkgs, ... }@inputs:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+        inherit system;
+      });
+    in
+    {
+      checks = forEachSupportedSystem ({ system, ... }: {
+        pre-commit-check = inputs.pre-commit-hooks.lib.${system}.run {
+          src = ./.;
+          hooks = {
+            # ruff.enable = true;
+            black.enable = true;
+            # ruff-format.enable = true;
+          };
+        };
+      });
+      devShells = forEachSupportedSystem ({ pkgs, system }: {
+        default = pkgs.mkShell {
+          venvDir = ".venv";
+          packages = with pkgs; [
+            python312
+            pre-commit
+          ] ++
+          (with pkgs.python312Packages; [
+            pip
+            venvShellHook
+          ]);
+          inherit (self.checks.${system}.pre-commit-check) shellHook;
+          buildInputs = self.checks.${system}.pre-commit-check.enabledPackages;
+        };
+      });
+    };
+}
