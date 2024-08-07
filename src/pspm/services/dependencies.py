b22fa91eb6b8a2870ab1921ee09845f17a56fbda
Jahn16 <jpdeoliveiraferreira@gmail.com>
2024-08-06 21:32:56 -0300

    feat: install dependency with add command

--- src/pspm/services/dependencies.py
+++ src/pspm/services/dependencies.py
@@ -4,8 +4,12 @@ from __future__ import annotations
 
 from pathlib import Path
 
+from rich import print
+
+from pspm.entities.installer import BaseInstaller, UVInstaller
 from pspm.entities.pyproject import Pyproject
 from pspm.entities.toml import Toml
+from pspm.errors.dependencies import InstallError
 
 
 def _get_pyproject() -> Pyproject:
@@ -14,12 +18,23 @@ def _get_pyproject() -> Pyproject:
     return Pyproject(parser)
 
 
+def _get_installer() -> BaseInstaller:
+    return UVInstaller()
+
+
 def add_dependency(package: str, group: str | None = None) -> None:
     """Add dependency to pyproject.
 
     :param package: Package to install
     :param group: Group to insert package
     """
+    installer = _get_installer()
+    try:
+        installer.install(package)
+    except InstallError:
+        print(f":boom: [red]Failed to install {package}[/red]")
+        return
+
     pyproject = _get_pyproject()
     if not group:
         pyproject.add_dependency(package)
