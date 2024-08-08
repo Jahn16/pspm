8e3c956b21ed5a273b262c41114363494cf9093a
Jahn16 <jpdeoliveiraferreira@gmail.com>
2024-08-08 08:39:39 -0300

    feat: add base pyproject class

--- src/pspm/entities/pyproject.py
+++ src/pspm/entities/pyproject.py
@@ -2,22 +2,50 @@
 
 from __future__ import annotations
 
+import abc
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     from pspm.entities.toml import BaseToml
 
 
-class Pyproject:
-    """Class for manipulating pyproject.toml file."""
+class BasePyproject:
+    """Manipulate pyproject.
+
+    Attributes:
+        toml_parser: Parser to be used for parsing TOML
+    """
 
     def __init__(self, toml_parser: BaseToml) -> None:
-        """Init Pyproject.
+        """Iniatilize BasePyproject.
 
         Args:
             toml_parser: Parser to be used for parsing TOML
         """
         self._parser = toml_parser
+
+    @abc.abstractmethod
+    def add_dependency(self, package: str) -> None:
+        """Add dependency to project.
+
+        Args:
+            package: Package to download
+        """
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def add_group_dependency(self, package: str, group: str) -> None:
+        """Add optional-dependency with group to project.
+
+        Args:
+            package: Package to install
+            group: Group that package will be inserted
+        """
+        raise NotImplementedError
+
+
+class Pyproject(BasePyproject):
+    """Class for manipulating pyproject.toml file."""
 
     def add_dependency(self, package: str) -> None:
         """Add dependency to project.
