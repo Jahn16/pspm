34283a40b99c50af90805d5fcaf8a38026cde71c
Jahn16 <jpdeoliveiraferreira@gmail.com>
2024-08-08 20:17:59 -0300

    feat: use package manager

--- src/pspm/services/dependencies.py
+++ src/pspm/services/dependencies.py
@@ -4,13 +4,11 @@ from __future__ import annotations
 
 from pathlib import Path
 
-from rich import print as rprint
-
 from pspm.entities.installer import BaseInstaller, UVInstaller
+from pspm.entities.package_manager import PackageManager
 from pspm.entities.pyproject import Pyproject
 from pspm.entities.resolver import BaseResolver, UVResolver
 from pspm.entities.toml import Toml
-from pspm.errors.dependencies import InstallError
 
 
 def _get_pyproject_path() -> str:
@@ -40,18 +38,7 @@ def add_dependency(package: str, group: str | None = None) -> None:
         package: Package to install
         group: Group to insert package
     """
-    installer = _get_installer()
-    try:
-        installer.install(package)
-    except InstallError:
-        rprint(f":boom: [red]Failed to install {package}[/red]")
-        return
-
-    pyproject = _get_pyproject()
-    if not group:
-        pyproject.add_dependency(package)
-    else:
-        pyproject.add_group_dependency(package, group)
-    resolver = _get_resolver()
-    output_file = f"requirements{'-' + group if group else ''}.lock"
-    resolver.compile(output_file, group)
+    package_manager = PackageManager(
+        _get_pyproject(), _get_installer(), _get_resolver()
+    )
+    package_manager.add_dependency(package, group)
