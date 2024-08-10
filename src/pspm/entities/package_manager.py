db795e26e7f60b6ac4621017a8aea4c3db98aa0f
Jahn16 <jpdeoliveiraferreira@gmail.com>
2024-08-09 22:03:42 -0300

    feat: split add dependency with group to other function

--- src/pspm/entities/package_manager.py
+++ src/pspm/entities/package_manager.py
@@ -4,8 +4,6 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-from pspm.errors.dependencies import InstallError
-
 if TYPE_CHECKING:
     from pspm.entities.installer import BaseInstaller
     from pspm.entities.pyproject import BasePyproject
@@ -32,30 +30,33 @@ class PackageManager:
         self._installer = installer
         self._resolver = resolver
 
-    def add_dependency(self, package: str, group: str | None = None) -> None:
+    def add_dependency(self, package: str) -> None:
         """Add dependency to pyproject.
 
         Args:
             package: Package to install
             group: Group to insert package
         """
-        try:
-            self._installer.install(package)
-        except InstallError:
-            return
-
-        group_file = "requirements-{}.lock"
-        if group:
-            self._pyproject.add_group_dependency(package, group)
-            self._resolver.compile(group_file.format(group), group)
-            return
-
         self._pyproject.add_dependency(package)
         main_file = "requirements.lock"
-        file_and_groups = [
-            (group_file.format(g), g)
-            for g in self._pyproject.get_extra_groups()
-        ]
+        group_file_format = "requirements-{}.lock"
+        all_groups = self._pyproject.get_extra_groups()
+        group_files = [group_file_format.format(g) for g in all_groups]
         self._resolver.compile(main_file)
-        for f, g in file_and_groups:
+        for f, g in zip(group_files, all_groups):
             self._resolver.compile(f, g)
+        self._installer.sync([main_file, *group_files])
+        self._installer.install(".")
+
+    def add_dependency_with_group(self, package: str, group: str) -> None:
+        """Add dependency to pyproject with group.
+
+        Args:
+            package: Package to install
+            group: Group to insert package
+        """
+        group_file = f"requirements-{group}.lock"
+        self._pyproject.add_group_dependency(package, group)
+        self._resolver.compile(group_file, group)
+        self._installer.sync([group_file])
+        self._installer.install(".")
