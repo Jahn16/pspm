eca4b1bb4978d3f66cc560cddce8285a8a860470
Jahn16 <jpdeoliveiraferreira@gmail.com>
2024-08-10 19:02:07 -0300

    feat: add virtual env class

---
+++ src/pspm/entities/virtual_env.py
@@ -0,0 +1,73 @@
+"""Module to define virtualenv classes."""
+
+import abc
+import subprocess
+from pathlib import Path
+
+from pspm.utils.bin_path import get_uv_path
+
+
+class BaseVirtualEnv(abc.ABC):
+    """Interacts with virtualenv."""
+
+    @abc.abstractmethod
+    def already_created(self) -> bool:
+        """Check if env is already created."""
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def create(self) -> None:
+        """Create virtualenv."""
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def activate(self) -> None:
+        """Activate virtualenv."""
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def run(self, command: str) -> None:
+        """Run command inside virtualenv.
+
+        Args:
+            command: Command to run
+        """
+        raise NotImplementedError
+
+
+class VirtualEnv(abc.ABC):
+    """Interacts with virtualenv."""
+
+    def __init__(self) -> None:
+        """Initialize BaseVirtualEnv."""
+        self._path = ".venv"
+
+    @abc.abstractmethod
+    def already_created(self) -> bool:
+        """Check if env is already created.
+
+        Returns:
+            Whether the env is created
+        """
+        p = Path(self._path)
+        return p.exists()
+
+    @abc.abstractmethod
+    def create(self) -> None:
+        """Create virtualenv."""
+        uv_path = get_uv_path()
+        subprocess.run([uv_path, "venv", self._path], check=False)
+
+    @abc.abstractmethod
+    def activate(self) -> None:
+        """Activate virtualenv."""
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def run(self, command: str) -> None:
+        """Run command inside virtualenv.
+
+        Args:
+            command: Command to run
+        """
+        raise NotImplementedError
