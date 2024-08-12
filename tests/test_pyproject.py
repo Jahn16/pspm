c832d7bb121b522cea5b68a8a8099145e79543a3
Jahn16 <jpdeoliveiraferreira@gmail.com>
2024-08-12 19:59:16 -0300

    tests: test remove dependency

--- tests/test_pyproject.py
+++ tests/test_pyproject.py
@@ -8,31 +8,38 @@ from pspm.entities.pyproject import Pyproject
 
 
 class DummyToml(BaseToml):
-    def __init__(self, path: str) -> None:
-        self.data = {
-            "project": {
-                "name": "test",
-                "dependencies": [
-                    "foo",
-                    "bar",
-                ],
-                "optional-dependencies": {
-                    "dev": ["developing"],
-                    "test": ["testing"],
-                },
-            },
-        }
+    def __init__(self, data: dict[str, Any]) -> None:
+        self.data = data
 
     def load(self) -> dict[str, Any]:
         return self.data
 
     def dump(self, data: dict[str, Any]) -> None:
-        return
+        self.data = data
+
+
+@pytest.fixture
+def data(requirements: dict[str, list[str]]) -> dict[str, Any]:
+    return {
+        "project": {
+            "name": "test",
+            "dependencies": requirements["main"],
+            "optional-dependencies": {
+                "dev": requirements["dev"],
+                "test": requirements["test"],
+            },
+        },
+    }
 
 
 @pytest.fixture()
-def toml_parser() -> BaseToml:
-    return DummyToml("foobar")
+def requirements() -> dict[str, list[str]]:
+    return {"main": ["foo", "bar"], "dev": ["developing"], "test": ["testing"]}
+
+
+@pytest.fixture
+def toml_parser(data: dict[str, Any]) -> BaseToml:
+    return DummyToml(data)
 
 
 @pytest.fixture()
@@ -47,6 +54,15 @@ def test_add_dependency(pyproject: Pyproject, toml_parser: BaseToml) -> None:
     pyproject.add_dependency(package)
     result = toml_parser.load()
     assert result["project"]["dependencies"] == expected_dependencies
+
+
+def test_remove_dependency(
+    pyproject: Pyproject, toml_parser: BaseToml
+) -> None:
+    package = "foo"
+    pyproject.remove_dependency(package)
+    result = toml_parser.load()
+    assert result["project"]["dependencies"] == ["bar"]
 
 
 def test_add_dependency_with_group(
@@ -68,6 +84,14 @@ def test_add_dependency_with_group(
         result["project"].get("optional-dependencies").get(group, [])
         == expected_dependencies
     )
+
+
+def test_remove_dependency_with_group(
+    pyproject: Pyproject, toml_parser: BaseToml
+) -> None:
+    pyproject.remove_dependency("developing", "dev")
+    result = toml_parser.load()
+    assert result["project"]["dependencies"] == []
 
 
 def test_dont_add_duplicate_dependency(
