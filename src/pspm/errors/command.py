860293e47e3c9677f1cc412fe9cebdc054179f13
Jahn16 <jpdeoliveiraferreira@gmail.com>
2024-08-13 20:09:52 -0300

    feat: add command errors

---
+++ src/pspm/errors/command.py
@@ -0,0 +1,28 @@
+"""Module for defining errors related to command execution."""
+
+
+class CommandError(Exception):
+    """Base error for command."""
+
+    def __init__(self, command: str) -> None:
+        """Initialize CommandError.
+
+        Args:
+            command: Command that failed to be executed.
+        """
+        self.command = command
+        self.message = f"Command {command} failed"
+        super().__init__(self.message)
+
+
+class CommandNotFoundError(CommandError):
+    """Comand bin was not found."""
+
+    def __init__(self, command: str) -> None:
+        """Initialize CommandNotFound.
+
+        Args:
+            command: Command that was not found
+        """
+        self.message = f"Command {command} was not found"
+        super().__init__(self.message)
