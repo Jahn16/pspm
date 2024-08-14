2719e1057c2ef69b7ebc17a3f514e4bd82dbbedf
Jahn16 <jpdeoliveiraferreira@gmail.com>
2024-08-14 20:06:07 -0300

    feat: add more depedency errors

--- src/pspm/errors/dependencies.py
+++ src/pspm/errors/dependencies.py
@@ -1,7 +1,11 @@
 """Module with errors related to dependency management."""
 
 
-class InstallError(Exception):
+class DependencyError(Exception):
+    """Base error for dependencies."""
+
+
+class InstallError(DependencyError):
     """Can't install package."""
 
     def __init__(self, package: str) -> None:
@@ -13,3 +17,19 @@ class InstallError(Exception):
         self.package = package
         self.message = f"Error installing package {package}"
         super().__init__(self.message)
+
+
+class SyncError(DependencyError):
+    """Can't sync dependencies."""
+
+    def __init__(self) -> None:
+        """Initialize SyncError."""
+        super().__init__("Could not sync dependencies")
+
+
+class ResolveError(DependencyError):
+    """Can't resolve dependencies."""
+
+    def __init__(self) -> None:
+        """Initialize SyncError."""
+        super().__init__("Error resolving dependencies")
