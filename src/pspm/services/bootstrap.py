dcfad5de471b08a35297d49fe731737f81c99675
Jahn16 <jpdeoliveiraferreira@gmail.com>
2024-08-15 20:44:42 -0300

    fix: set hatch tool package

--- src/pspm/services/bootstrap.py
+++ src/pspm/services/bootstrap.py
@@ -14,8 +14,7 @@ from rich import print as rprint
 from pspm.utils.commands import get_git_user
 
 
-def _create_package_structure(base_path: Path, name: str) -> None:
-    safe_name = name.lower().replace("-", "_")
+def _create_package_structure(base_path: Path, safe_name: str) -> None:
     package_init = base_path / "src" / safe_name / "__init__.py"
     package_init.parent.mkdir(parents=True, exist_ok=True)
     package_init.write_text("")
@@ -39,10 +38,12 @@ def bootstrap_project(
     env = Environment(loader=PackageLoader("pspm"), autoescape=True)
     template_names = env.list_templates()
     author = get_git_user()
+    safe_package_name = name.lower().replace("-", "_")
     for template_name in template_names:
         template = env.get_template(template_name)
         result = template.render(
             name=name,
+            safe_name=safe_package_name,
             description=description,
             installable=installable,
             author=author,
@@ -50,7 +51,7 @@ def bootstrap_project(
         file_name = template_name.replace(".j2", "")
         file_path = path / file_name
         file_path.write_text(result, encoding="utf-8")
-    _create_package_structure(path, name)
+    _create_package_structure(path, safe_package_name)
     rprint(
         f"Initialized project [blue]{name}[/blue] "
         + (f"in [blue]{path.name}[/blue]" if path.name else "")
