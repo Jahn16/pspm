e55c71aabf4bfd2f7a54e91f9467769291dd1b51
Jahn16 <jpdeoliveiraferreira@gmail.com>
2024-08-15 08:31:39 -0300

    feat: add author to pyproject

--- src/pspm/services/bootstrap.py
+++ src/pspm/services/bootstrap.py
@@ -2,15 +2,21 @@
 
 from __future__ import annotations
 
-from pathlib import Path
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from pathlib import Path
+
 
 from jinja2 import Environment, PackageLoader
 from rich import print as rprint
 
+from pspm.utils.commands import get_git_user
 
-def _create_package_structure(name: str) -> None:
+
+def _create_package_structure(base_path: Path, name: str) -> None:
     safe_name = name.lower().replace("-", "_")
-    package_init = Path(f"src/{safe_name}/__init__.py")
+    package_init = base_path / "src" / safe_name / "__init__.py"
     package_init.parent.mkdir(parents=True, exist_ok=True)
     package_init.write_text("")
 
@@ -32,15 +38,19 @@ def bootstrap_project(
     path.mkdir(exist_ok=True)
     env = Environment(loader=PackageLoader("pspm"), autoescape=True)
     template_names = env.list_templates()
+    author = get_git_user()
     for template_name in template_names:
         template = env.get_template(template_name)
         result = template.render(
-            name=name, description=description, installable=installable
+            name=name,
+            description=description,
+            installable=installable,
+            author=author,
         )
         file_name = template_name.replace(".j2", "")
         file_path = path / file_name
         file_path.write_text(result, encoding="utf-8")
-    _create_package_structure(name)
+    _create_package_structure(path, name)
     rprint(
         f"Initialized project [blue]{name}[/blue] "
         + (f"in [blue]{path.name}[/blue]" if path.name else "")
